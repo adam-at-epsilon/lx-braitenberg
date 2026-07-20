a13ca1a55863b85a608bde0a0d01c6b54a2d6950
Adam S <adam.at.epsilon@gmail.com>
2026-07-20 07:41:26 +0200

    x**2 matrices. Robot diverges right

--- packages/solution/connections.py
+++ packages/solution/connections.py
@@ -7,11 +7,14 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
 
     x_max = shape[1]
-    y_max = shape[0]
+    y_max = shape[0] # Y counts from 0 (top) to y_max (bottom)
+
     print(f"L type: {type(res[0,0])}")
-    for x in range(0, int(x_max / 2)):
+    for x in range(0, x_max):
+        x_frac = (float(x) / x_max)
+        y_frac = (x_frac - 1.0) ** 2
         for y in range(0, y_max):
-            res[y, x] = matrix_weight
+            res[y, x] = y_frac
     
     return res
 
@@ -22,8 +25,10 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     x_max = shape[1]
     y_max = shape[0]
 
-    for x in range(int(x_max / 2), x_max):
+    for x in range(0, x_max):
+        x_frac = float(x) / x_max
+        y_frac = x_frac ** 2
         for y in range(0, y_max):
-            res[y, x] = matrix_weight
+            res[y, x] = y_frac
     
     return res
